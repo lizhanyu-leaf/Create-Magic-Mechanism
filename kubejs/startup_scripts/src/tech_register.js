be6cca9c76e60498f1d8a8a49032e7cb05869725
lizhanyu-leaf <3854186771@qq.com>
2026-07-10 07:08:17 +0800

    准备重写科技系统

---
+++ kubejs/startup_scripts/src/tech_register.js
@@ -0,0 +1,13 @@
+TechSystemEvents.registerTech(event => {
+
+    event.create("cmm:mechancal_tech_pack")
+        .icon('minecraft:brown_wool')
+        .itemLang('zh_cn', '知识: 机械科技包')
+        .lang('zh_cn', '机械科技包')
+
+    event.create('cmm:steel_tech_pack')
+        .icon('minecraft:light_gray_wool')
+        .itemLang('zh_cn', '知识: 钢材科技包')
+        .lang('zh_cn', '钢材科技包')
+
+})
